0f1b63a50a3340f9449a203957c40f7f75ccf018
Yunus Emre <yunus1481emre@gmail.com>
2024-11-07 22:48:25 +0300

    update

--- Core/DTOs/User/UserLoginDTO.cs
+++ Core/DTOs/User/UserLoginDTO.cs
@@ -3,7 +3,6 @@
     public class UserLoginDTO
     {
         public int UserId { get; set; }
-        public string Username { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
     }
